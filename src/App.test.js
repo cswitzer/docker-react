218a2c94663d230dbd4caafcd0b246a638f57e11
cswitzer <chasehillig@yahoo.com>
2025-03-06 12:44:31 -0600

    fix failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 test("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(
-        /Hi there! I'm a React app. I am also a Docker container./i
+        /I was changed on the feature branch/i
     );
     expect(linkElement).toBeInTheDocument();
 });
